0600932af7b671cdaa1e504af49ceee392985784
Nishith <nishith.one@gmail.com>
2025-05-02 14:29:36 +0530

    Implemented Logging using winston logger

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const apiRoutes = require('./routes')
-const { PORT } = require("./config");
+const apiRoutes = require("./routes");
+const { ServerConfig, Logger } = require("./config");
 
 const app = express();
 
 app.use("/api", apiRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Successfully started the server on PORT : ${PORT}`);
+app.listen(ServerConfig.PORT, () => {
+  console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+  Logger.info("Successfully started the server");
 });
