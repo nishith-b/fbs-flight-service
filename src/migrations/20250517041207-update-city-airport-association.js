c2ed9dd56114ae0deef7996d67ed4b95df7252b6
Nishith <nishith.one@gmail.com>
2025-05-17 11:28:32 +0530

    Added new migration that can add a constraints in the column of airport table ``` npx sequelize migration:generate --name update-city-airport-association
    ```

---
+++ src/migrations/20250517041207-update-city-airport-association.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addConstraint("Airports", {
+      name: "city_fkey_constraints",
+      fields: ["cityId"],
+      type: "foreign key",
+      references: {
+        table: "Cities",
+        field: "id",
+      },
+      onUpdate: "CASCADE",
+      onDelete: "CASCADE",
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeConstraint("Airports", "city_fkey_constraint");
+  },
+};
--- src/models/city.js
+++ src/models/city.js
@@ -8,7 +8,11 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      this.hasMany(models.Airport, {
+        foreignKey: "cityId",
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      });
     }
   }
   City.init(
@@ -16,7 +20,7 @@ module.exports = (sequelize, DataTypes) => {
       name: {
         type: DataTypes.STRING,
         allowNull: false,
-        unique:true
+        unique: true,
       },
     },
     {
